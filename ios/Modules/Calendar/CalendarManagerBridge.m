2b665cb5a7af08086e975adc572ffc87647e8a6d
Adonai Pinheiro - Smiles <72467499+adonaipinheirosmiles@users.noreply.github.com>
2023-02-14 11:39:38 -0300

    Initial commit

---
+++ ios/Modules/Calendar/CalendarManagerBridge.m
@@ -0,0 +1,14 @@
+//
+//  CalendarManagerBridge.m
+//  COEMobile
+//
+//  Created by Adonai Junio Pinheiro on 09/02/23.
+//
+
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(CalendarManager, NSObject)
+
+RCT_EXTERN_METHOD(increment: (RCTResponseSenderBlock)callback)
+
+@end
